2ca91657d00e64b7631357be00606c9ce05f13b1
Renzo <renzin@gmail.com>
2021-04-27 20:08:53 +0000

    changing formula for pass check

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    if (i % 2 != 0):
-        print("{}".format(chr(i + 97)), end="")
-    else:
-        print("{}".format(chr(i + 65)), end="")
+for i in range(ord("z")90, ord("a")-1, -1):
+    if (i % 2) == 0:
+        i = i + 32
+    print("{:c}".format(i), end='')
