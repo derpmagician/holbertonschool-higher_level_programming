292a62dd9086616834df5353ea97437a365b3efb
Renzo <renzin@gmail.com>
2021-04-28 03:04:29 +0000

    fixing ex 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord("z")90, ord("a")-1, -1):
-    if (i % 2) == 0:
-        i = i + 32
-    print("{:c}".format(i), end='')
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
+    else:
+        print('{:c}'.format(i - 32), end='')
