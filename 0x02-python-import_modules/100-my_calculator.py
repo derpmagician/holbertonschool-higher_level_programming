9d49131f09618e5c61dbc6c72e17c2a97bdce6c8
Renzo <renzin@gmail.com>
2021-04-29 18:05:36 +0000

    changing import location

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
 if __name__ == '__main__':
     """
     Performs basic operations between two numbers + - * /
     Input the opeation and the 2 numbers.
     """
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
