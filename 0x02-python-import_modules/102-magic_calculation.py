839cf0017ff9d267e0fb66e272235193f2a4fbb4
Renzo <renzin@gmail.com>
2021-04-29 21:54:20 +0000

    fixing ecuation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,12 +2,9 @@
 from magic_calculation_102 import add, sub
 
 
-def magic_calculation(a, b):
-    if (a < b):
-        c = add(a, b)
-
-        for i in range(4, 6):
-            c = add(c, i)
-            return c
-        else:
-            return sub(a, b)
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return a * b - c
