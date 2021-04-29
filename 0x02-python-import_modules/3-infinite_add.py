9fe76ebf05e99a637c3f91872e60edaf5b9751ec
Renzo <renzin@gmail.com>
2021-04-29 16:35:46 +0000

    changing var name

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,9 @@ import sys
 
 if __name__ == '__main__':
     av = sys.argv
-    l_av = len(av)
+    nargs = len(av)
     sum = 0
-    if l_av > 1:
-        for i in range(1, l_av):
+    if nargs > 1:
+        for i in range(1, nargs):
             sum += int(av[i])
     print(sum)
