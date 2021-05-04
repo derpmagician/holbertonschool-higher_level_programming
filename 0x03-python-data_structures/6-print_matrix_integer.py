57df6ee1ea3beaab6845e101560f51a37c34f66c
Renzo <renzin@gmail.com>
2021-05-04 20:26:24 +0000

    relocating print func

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
         for i in fila:
             print("{:d}".format(i), end=" " if fila[-1] != i else "")
-    print()
+        print()
