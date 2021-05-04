7e6646f2126fd3a3a6c7c50b20963c3f3e1a1057
Renzo <renzin@gmail.com>
2021-05-04 20:20:57 +0000

    fixing misspelling

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
         for i in fila:
-            print("{:d}".format(i), end=" " if row[-1] != i else "")
+            print("{:d}".format(i), end=" " if fila[-1] != i else "")
     print()
