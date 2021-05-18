367c7fe067433343c06c1c7a1bf1b14c3a5f2cb8
Renzo <renzin@gmail.com>
2021-05-18 12:20:11 +0000

    adding exercises

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A Square Class
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """
+        The __init__ method initializes the size of the square.
+        Attributes:
+            size (int): The size of the square.
+        """        self.__size = size
