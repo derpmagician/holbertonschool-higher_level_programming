ed60464441aaade0ab830e5cc5dae290351ba0b7
Renzo <renzin@gmail.com>
2021-05-18 20:52:07 +0000

    fixing desc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,11 @@ A Square Class
 
 
 class Square:
-
+    """ represents a square """
     def __init__(self, size):
         """
         The __init__ method initializes the size of the square.
         Attributes:
             size (int): The size of the square.
-        """        self.__size = size
+        """
+        self.__size = size
