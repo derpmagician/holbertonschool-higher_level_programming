6f83b97568e1e12a78548fe1f7a716fc7dad6250
Renzo <renzin@gmail.com>
2021-05-18 21:42:34 +0000

    adding node class

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class Node:
+    """ `Square` class """
+    def __init__(self, data, next_node=None):
+        """ Initializes private instance
+            positive integer `__data` and `__next_node` """
+        if type(data) is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = data
+
+        if type(next_node) is not Node and \
+           next_node is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = next_node
+
+    @property
+    def data(self):
+        """ getter for `__data` """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """ setter for `__data` """
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """ getter for `__next_node` """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """ setter for `__next_node` """
+        if type(value) is not Node and \
+           value is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
