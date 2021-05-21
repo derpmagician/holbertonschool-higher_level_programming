1ba761b372bd71fc782cc316a5479c20dbb3e7a9
derpmagician <renzin@gmail.com>
2021-05-21 11:35:49 -0500

    adding desc

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,9 +5,22 @@ Defines lazy_matrix function
 import numpy
 
 
+
 def lazy_matrix_mul(m_a, m_b):
+    """Multiplies one matrix by another.
+    Args:
+        m_a: the first matrix
+        m_b: the second matrix
+    Returns:
+        matrix: the product
+    Raises:
+        TypeError: If m_a or m_b are not lists.
+        TypeError: If m_a or m_b are not lists of lists.
+        ValueError: If m_a or m_b are empty lists/matrices.
+        TypeError: If m_a or m_b contain a non int/float.
+        TypeError: If m_a or m_b are not rectangular.
+        ValueError: If m_a or m_b can't be multiplied.
     """
-    calculates the matrix multiplication of two matrix
-    using the numpy module
-    """
+
+
     return numpy.matmul(m_a, m_b)
