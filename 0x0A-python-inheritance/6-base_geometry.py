54ce754c251e7431efaf86b0f340ef2238f5c042
derpmagician <renzin@gmail.com>
2021-06-01 10:20:45 -0500

    updating descriptions

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,5 +5,8 @@ A class with a not implemented method
 
 #!/usr/bin/python3
 class BaseGeometry:
+    """
+    A super class that implements geometrical shapes
+    """
     def area(self):
         raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,5 +5,22 @@ A class with a not implemented method
 
 #!/usr/bin/python3
 class BaseGeometry:
+    """
+    A super class that implements geometrical shapes
+    """
     def area(self):
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Checks a integer value
+        Raises:
+            TypeError: If `value` isn't a integer.
+            ValueError: If `value` is less than or equal to zero.
+        """
+
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
