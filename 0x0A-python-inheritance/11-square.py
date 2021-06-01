54176f7a25d4b99582333af86b7a684f6c00fd8c
derpmagician <renzin@gmail.com>
2021-06-01 14:33:02 -0500

    adding ex

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,4 +7,7 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size 
+        self.__size = size
+
+    def __str__(self):
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
