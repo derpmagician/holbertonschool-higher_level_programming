7bc3771757915e00d638d69787a26e9456a3554e
derpmagician <renzin@gmail.com>
2021-06-01 16:12:51 -0500

    adding ex 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Adds new atribute if possible
+"""
+
+
+def add_attribute(object, attribute, value):
+    """adds a new attribute to an object if its possible"""
+    if hasattr(object, "__dict__"):
+        setattr(object, attribute, value)
+    else:
+        raise TypeError("can't add new attribute"
