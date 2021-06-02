28080a7f087d02153fe13e258156b1d140a209ab
derpmagician <renzin@gmail.com>
2021-06-02 18:57:36 -0500

    adding desc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """adds all arguments to a Python list"""
-
-
 from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    ls = load_from_json_file(filename)
-except:
-    ls = []
+    js_list = load_from_json_file(filename)
+except Exception as e:
+    js_list = []
+    with open(filename, 'w+') as f:
+        pass
 
-ls += [arg for arg in argv[1:]]
+for arg in argv[1:]:
+    json_list.append(arg)
+
+
 save_to_json_file(ls, filename)
