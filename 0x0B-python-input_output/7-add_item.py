8d7a3634d7e705a5f26d5c02f75a8ed45a0a1d6d
derpmagician <renzin@gmail.com>
2021-06-02 18:41:34 -0500

    adding desc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,16 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
+filename = "add_item.json"
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
+try:
+    json_list = from_json_file(filename)
+except Exception as e:
+    json_list = []
+    with open(filename, 'w+') as f:
+        pass
 
-save_to_json_file(obj_json_file, 'add_item.json')
+for arg in argv[1:]:
+    json_list.append(arg)
+
+to_json_file(json_list, filename)
