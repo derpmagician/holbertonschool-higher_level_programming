59907a0dce9ae510f04ce923941a477d5e4d413b
derpmagician <renzin@gmail.com>
2021-06-02 17:57:40 -0500

    adding desc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Function"""
+
 def number_of_lines(filename=""):
+    """ writes a string to a text file and returns the len of chars"""
     with open(filename, encoding='utf-8') as f:
         return len(f.readlines())
