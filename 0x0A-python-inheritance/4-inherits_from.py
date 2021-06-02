8bbe0d64240627d3f7ff100faa14d069c8b5e456
derpmagician <renzin@gmail.com>
2021-06-01 19:02:33 -0500

    adding sheabang

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,8 @@
-nherits_from(obj, a_class):
+#!/usr/bin/python3
+"""Define inherits_from function"""
+
+
+def inherits_from(obj, a_class):
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
