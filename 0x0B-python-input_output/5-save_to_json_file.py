d4f75331fcdb7c654e846d780f448f5493643dbd
derpmagician <renzin@gmail.com>
2021-06-02 18:20:23 -0500

    adding desc

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"Method Module"
 from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
+     """function that writes an Object to a text file"""
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(dumps(my_obj))
