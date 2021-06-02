42eeec18fc9486f85b245f9863e41e7750d27dc7
derpmagician <renzin@gmail.com>
2021-06-02 18:25:20 -0500

    adding desc

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"Method Module"
 from json import loads
 
 
 def load_from_json_file(filename):
+    """function that loads an Object"""
     with open(filename, encoding='utf-8') as f:
         return loads(f.read())
