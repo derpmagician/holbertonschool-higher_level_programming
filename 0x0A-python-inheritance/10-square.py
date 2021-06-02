9d1dec8aa18634dfd2bf40d8025f6b3524691fce
derpmagician <renzin@gmail.com>
2021-06-01 19:26:29 -0500

    adding desc

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+Define class BaseGeometry
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    Class initlization
+    """
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
