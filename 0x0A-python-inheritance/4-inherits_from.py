800f501fa7c9a647d92e33c09d57a993db3f70ce
derpmagician <renzin@gmail.com>
2021-06-01 19:07:11 -0500

    adding desc

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,10 @@
 
 
 def inherits_from(obj, a_class):
+    """
+    "Detecs if obj is instance of a class that inherited from
+    a_class
+    """
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
