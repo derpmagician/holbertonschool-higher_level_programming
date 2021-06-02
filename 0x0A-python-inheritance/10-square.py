6e95a4fbfab520cd9344c60b344c8d7091f2dc8e
derpmagician <renzin@gmail.com>
2021-06-01 19:35:36 -0500

    adding desc

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,18 +4,17 @@ Define class BaseGeometry
 """
 
 
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """
-    Class initlization
-    """
+    """ Square class that inherits from Rectangle """
     def __init__(self, size):
+        """ Initializes validated private instance __width """
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
-        self.__size = size 
 
     def area(self):
-        """"Returns the area of the square"""
+        """" Returns the area of the square """
         return self.__size ** 2
