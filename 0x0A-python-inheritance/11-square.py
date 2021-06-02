e9a8fca35f4fe66a42dbe15f91783b955f7f4daa
derpmagician <renzin@gmail.com>
2021-06-01 19:38:22 -0500

    adding desc

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+"""
+Define Square class
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Initializes a square"""
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """return square area"""
+        return self.__size ** 2
 
     def __str__(self):
         return '[Square] ' + str(self.__size) + '/' + str(self.__size)
