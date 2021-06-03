644fdfc69d3d168ee14ae5d0e37cbf3344c04965
derpmagician <renzin@gmail.com>
2021-06-02 19:50:23 -0500

    adding ex 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Initilization"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        class_d = self.__dict__
+        sel_d = dict()
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary"""
+        if attrs is None:
+            return self.__dict__
+        my_dict = dict()
+        for attr in attrs:
+            if attr in self.__dict__:
+                my_dict[attr] = self.__dict__[attr]
+        return my_dict
+
+    def reload_from_json(self, json):
+        """Replace the attributes of the instance"""
+        for key in json:
+            self.__dict__[key] = json[key]
