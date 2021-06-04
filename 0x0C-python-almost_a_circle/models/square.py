c87934cbaf435e9f67941b348750954889d86291
derpmagician <renzin@gmail.com>
2021-06-04 16:20:40 -0500

    adding square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+The squares module contains Rectangle as the superclass
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A representation of a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes the square"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
