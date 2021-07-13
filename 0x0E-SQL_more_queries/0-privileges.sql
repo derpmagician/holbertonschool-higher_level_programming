a0a17cc4d7d27c5dd2930d6dfc292c5c14d2e02e
derpmagician <renzin@gmail.com>
2021-07-13 12:12:01 -0500

    fixing desc

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- lists all privileges of the users user_0d_1 and user_0d_2 on your server.
+-- lists all privileges of the 2 users on the server.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
