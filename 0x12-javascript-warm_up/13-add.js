73d724f2395e4c142242e37c809b91275ebc8098
derpmagician <renzin@gmail.com>
2021-08-02 14:30:24 -0500

    exporting a function

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // Write a function that returns the addition of 2 integers, from outside
 
-const add = require('./13-add').add;
-console.log(add(3, 5));
+exports.add = function (a, b) {
+  return a + b;
+};
