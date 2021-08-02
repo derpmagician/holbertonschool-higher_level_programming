e4b5acf48a936b2721303b30285a4343329b3c07
derpmagician <renzin@gmail.com>
2021-08-02 12:49:08 -0500

    fixing misspelling

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints 3 lines
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let items in myArray) {
   console.log(myArray[items]);
 }
