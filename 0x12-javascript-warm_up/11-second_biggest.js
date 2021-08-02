55d79558082cce545300ba0dbb92fc8006cfe7df
derpmagician <renzin@gmail.com>
2021-08-02 13:29:44 -0500

    adding ex11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a script that searches the second biggest integer in the list of arguments.
+
+const myargs = process.argv.slice(2)
+const mylength = myargs.length;
+
+function compareN(a, b) {
+  return b - a;
+}
+
+console.log(mylength < 2 ? 0 : myargs.sort(compareN)[1]);
