48dcfddf92c70df573ac4e337e70b3142c28b81f
derpmagician <renzin@gmail.com>
2021-08-02 15:00:58 -0500

    adding function

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
-
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
