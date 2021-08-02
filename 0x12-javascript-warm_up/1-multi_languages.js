eb1daf8e26fab195a6a3b2515b63e2af296be9c1
derpmagician <renzin@gmail.com>
2021-08-02 12:16:44 -0500

    fixing misspelling

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // prints 3 lines:
 
-const msg = 'C is fun\nPython is cool\nJavascript is amazing';
+const msg = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(msg);
