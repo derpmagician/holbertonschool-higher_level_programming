8791b72e71052d4b009a5708497514a6fe91ab2e
derpmagician <renzin@gmail.com>
2021-08-02 14:51:13 -0500

    fixing loop

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Write a function that executes x times a function.
 
-exports.callMeMoby = function (x, theFunction){
-  while (x > 0) theFunction;
-  x--;
-}
+exports.callMeMoby = function (x, theFunction) {
+  while (x > 0) theFunction x--;
+};
