b55a8f5a6c9050b833d5ea79ebb372485bb95087
derpmagician <renzin@gmail.com>
2021-08-03 12:07:31 -0500

    adapting code to semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,11 +2,11 @@
 // Write a function that returns the reversed version of a list:
 
 exports.esrever = function (list) {
-  const reverse_list = [];
+  const reverseList = [];
 
   for (let i = list.length - 1; i >= 0; --i) {
-    reverse_list .push(list[i]);
+    reverseList.push(list[i]);
   }
 
-  return reverse_list ;
+  return reverseList;
 };
