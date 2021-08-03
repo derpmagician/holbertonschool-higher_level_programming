faaac52c746b7aace02a1f82ffc76bfdd5504a60
derpmagician <renzin@gmail.com>
2021-08-03 13:05:56 -0500

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,7 @@
 // Write a script that concats 2 files.
 
 const fs = require('fs');
-const arg = process.argv.slice(2)
+const arg = process.argv.slice(2);
 
 const file1 = fs.readFileSync(arg[1], 'utf8');
 const file2 = fs.readFileSync(arg[2], 'utf8');
