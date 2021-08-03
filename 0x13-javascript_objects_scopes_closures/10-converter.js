263beae9a00e3ec5e5b8ae56a20a8c9a47cce93d
derpmagician <renzin@gmail.com>
2021-08-03 12:08:56 -0500

    adapting code to semistandard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Converts a number from base 10 to another base passed as argument
- 
+
 exports.converter = function (base) {
   function myConverter (number) {
     return number.toString(base);
