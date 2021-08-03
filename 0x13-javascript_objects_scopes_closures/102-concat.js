599d3584662416206c25046ba88d4ea5ac6a0e1b
derpmagician <renzin@gmail.com>
2021-08-03 13:14:52 -0500

    fixing arg index

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,9 @@
 // Write a script that concats 2 files.
 
 const fs = require('fs');
-const arg = process.argv.slice(2);
+const myarg = process.argv.slice(1);
 
-const file1 = fs.readFileSync(arg[1], 'utf8');
-const file2 = fs.readFileSync(arg[2], 'utf8');
+const file1 = fs.readFileSync(myarg[1], 'utf8');
+const file2 = fs.readFileSync(myarg[2], 'utf8');
 
-fs.writeFileSync(arg[3], file1 + file2);
+fs.writeFileSync(myarg[3], file1 + file2);
