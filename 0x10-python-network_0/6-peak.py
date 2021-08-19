83a5f612c8c3dba1809b66d3fe96959998787210
derpmagician <renzin@gmail.com>
2021-08-19 16:43:01 -0500

    [UPDATE] replacing tabs with spaces

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,21 +5,21 @@ finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
-	""" Finds a peak in a list of unsorted integers. """
+    """ Finds a peak in a list of unsorted integers. """
 
-	my_list = list_of_integers
-	l = 0; r = len(my_list) - 1
+    my_list = list_of_integers
+    l = 0; r = len(my_list) - 1
 
-	if not my_list:
-		return None
+    if not my_list:
+        return None
 
-	while (l < r):
-		center = (l + r) // 2
-		if my_list[center] < my_list[center + 1]:
-			l  = center + 1
-		else:
-			r = center
+    while (l < r):
+        center = (l + r) // 2
+        if my_list[center] < my_list[center + 1]:
+            l  = center + 1
+        else:
+            r = center
 
-	return my_list[l]
+    sreturn my_list[l]
 
 
