7b3bc5544d76a16fcf654ccc799bba4b2554c1ce
derpmagician <renzin@gmail.com>
2021-08-20 09:19:41 -0500

    [ADD] file

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the URL with the email as a parameter, and displays the
+body of the response (decoded in utf-8).
+"""
+
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+
+if __name__ == "__main__":
+	url = argv[1]
+    data = urlencode({ 'email': argv[2] }).encode('ascii')
+    req = Request(url, data)
+
+    with urlopen(req) as res:
+        print(res.read().decode('utf-8'))
