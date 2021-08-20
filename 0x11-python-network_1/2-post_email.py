9e9797d07cc834ba7abcdc938cee50ac0e45b037
derpmagician <renzin@gmail.com>
2021-08-20 12:22:12 -0500

    [UPDATE] deleting trailing white space

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ from urllib.parse import urlencode
 
 if __name__ == "__main__":
     url = argv[1]
-    data = urlencode({ 'email': argv[2] }).encode('ascii')
+    data = urlencode({'email': argv[2]}).encode('ascii')
     req = Request(url, data)
 
     with urlopen(req) as res:
