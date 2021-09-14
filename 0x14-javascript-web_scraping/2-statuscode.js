3981ce52a80a3ca9cfd9dc32003a1d58969e1c7f
derpmagician <renzin@gmail.com>
2021-09-14 11:10:17 -0500

    [ADD] file

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Display the status code of a GET request.
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (_error, res) => {
+  console.log('code:', res.statusCode);
+});
