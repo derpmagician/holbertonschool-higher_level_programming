996e8d15f0446fbce5e8362c33535daa263ab7b8
derpmagician <renzin@gmail.com>
2021-09-14 12:41:07 -0500

    [UPDATE] semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (_error, response, body) => {
-  if (!_error) {console.log('code:', response && response.statusCode)};
+  if (!_error) { console.log('code:', response && response.statusCode); }
 });
