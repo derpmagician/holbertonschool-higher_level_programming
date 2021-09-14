a66c4c4dc0997c8c58a704f16d9f933e99b2767f
derpmagician <renzin@gmail.com>
2021-09-14 12:44:41 -0500

    [ADD] file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Prints the title of a Star Wars movie equal to a given integer.
+
+const request = require('request');
+const Wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+const url = process.argv[2];
+
+request(url, function (error, _res, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      for (const char of film.characters) {
+        if (char === Wedge) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
