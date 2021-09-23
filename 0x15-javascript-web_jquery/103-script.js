60ac67f34ad6f6666f86c2d355c17af71f238fdf
derpmagician <renzin@gmail.com>
2021-09-22 19:17:39 -0500

    [ADD] file

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,14 @@
+const url = 'https://fourtonfish.com/hellosalut/?lang=';
+
+$(() => {
+  $('INPUT#btn_translate').click(() =>
+    $.get(url + $('INPUT#language_code').val(), (data) =>
+      $('DIV#hello').html(data.hello))
+  );
+  $('INPUT#language_code').keypress((e) => {
+    if (e.which === 13) {
+      $.get(url + $('INPUT#language_code').val(), (data) =>
+        $('DIV#hello').html(data.hello));
+    }
+  });
+});
